3d0bd51c1622f1bbb693430d4c38f495c80a3f47
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-05-19 19:33:25 +0700

    Add precompiled headers.

---
+++ FinalYearProject/pch.h
@@ -0,0 +1,13 @@
+#ifndef PCH_H
+#define PCH_H
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif//WIN32_LEAN_AND_MEAN
+
+#include "Resource.h"
+
+#endif//PCH_H
