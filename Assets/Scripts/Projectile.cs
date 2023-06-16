bde1d0d5216fb6ffcda7386f0c55f5c5798ac1b6
Wyxather <79754485+Wyxather@users.noreply.github.com>
2023-06-16 20:53:45 +0700

    Attach pan sprite to projectile.

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -2,4 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour {}
+public class Projectile : MonoBehaviour 
+{
+    Rigidbody2D rigidBody2D;
+
+    void  Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float angle = Mathf.Atan2(rigidBody2D.velocity.y, rigidBody2D.velocity.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle - 40f, Vector3.forward);
+    }
+}
